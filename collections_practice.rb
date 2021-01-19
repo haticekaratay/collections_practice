ef6a17b04b6e3c3c409bc95754516219c8ec30b7
Hatice <haticekaraty@gmail.com>
2021-01-18 21:26:31 -0500

    commit tree 3890412fec3c7f9f5064aea6eceb8aa4e16ab17a

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
 
+def sort_array_asc(numArray)
+    numArray.sort do |a,b|
+        a <=> b
+    end
+end
+
+def sort_array_desc(numArray)
+    numArray.sort do |b,a|
+        a <=> b
+    end
+end
+
+def sort_array_char_count(stringArray)
+    stringArray.sort do |a,b|
+        a.length <=> b.length
+    end   
+end
+
+def swap_elements(array)
+    array[1],array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(numArray)
+    numArray.reverse()
+end
+
+def kesha_maker(stringArray)
+    stringArray.each{|word|
+        word[2] = "$"
+    }
+    stringArray
+end
+
+def find_a(stringArray)
+    a_word_array = []
+    stringArray.each{|word|
+        if word.start_with?("a")
+            a_word_array.push(word)
+        end
+    }
+    a_word_array
+end
+
+def sum_array(numArray)
+    #numArray.inject(:+)
+    sum = 0
+    numArray.each{|num| sum += num}
+    sum
+end    
+
+def add_s(stringArray)
+    stringArray.each_with_index.collect {|string,index|
+        if index != 1
+            string<<"s"
+        else
+            string
+        end
+    }
+end
